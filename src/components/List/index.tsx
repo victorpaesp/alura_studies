2b17591f0ff476f8eea17f542a2e9574fcfa30a3
Victor Paes <victorpaesp@gmail.com>
2023-06-15 14:07:51 -0300

    chore: aula-2-componentização

---
+++ src/components/List/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+function List() {
+    const tasks = [{
+        task: 'React',
+        time: '02:00:00'
+    }, {
+        task: 'JavaScript',
+        time: '01:00:00'
+    }, {
+        task: 'TypeScript',
+        time: '03:00:00'
+    }]
+    return (
+        <aside>
+            <h2> Estudos do dia </h2>
+            <ul>
+                {tasks.map((item, index) => (
+                    <li key={index}>
+                        <h3> {item.task} </h3>
+                        <p> {item.time} </p>
+                    </li>
+                ))}
+            </ul>
+        </aside>
+    )
+}
+
+export default List;
